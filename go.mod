40a73ddd6d9103ca161436026b1a4f8e20803694
surya_dwipa <dwipasurya5@gmail.com>
2023-05-09 10:23:09 +0800

    update data go mod

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/suryadwipa/app-say-hello-2.git
 
 go 1.17
+
+require github.com/ProgrammerZamanNow/go-say-hello v1.5.0
--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	go_say_hello "github.com/ProgrammerZamanNow/go-say-hello"
+)
+
+func main() {
+	fmt.Println(go_say_hello.SayHello())
 }
